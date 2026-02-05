edba80b5fc5bbf246fafb8fcf5fbb5364a62bab3
Christian Hemprich <hemprich.c@gmail.com>
2026-02-04 19:28:46 -0800

    Enhance comments for maze solving logic
    Added comments to clarify base and recursive cases in the maze solving algorithm, considered possibility that entrance (0,0) is not a valid open path

--- LabWeek2.java
+++ LabWeek2.java
@@ -2,20 +2,33 @@
 // Assumes entrance at top left corner of 2D array and exit at bottom right corner of 2D array
 // 0 = path, 1 = wall, 2 = checked space, 3 = final solved path
 
+// Basic 2D array maze solving tool using recursion
+// Assumes entrance at top left corner of 2D array and exit at bottom right corner of 2D array
+// 0 = path, 1 = wall, 2 = checked space, 3 = final solved path
+
+// base case
+// 1) exit reached
+// 2) row/column index not within dimensions of 2D array
+// 3) dead end reached
+// 4) if (0,0) is not a 0
+
+// recursive case
+// 1) when you reach a new unchecked path
+
 public class MazeSolver {
     public static void main(String[] args) {
         // 2D Array to represent initial maze layout (example, can be modified)
         int[][] maze = {
-                {0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-                {1, 1, 0, 0, 0, 1, 1, 1, 0, 0},
-                {0, 0, 0, 1, 1, 0, 0, 0, 1, 0},
-                {0, 1, 1, 1, 0, 0, 1, 0, 0, 0},
-                {0, 1, 0, 0, 0, 1, 1, 1, 1, 1},
-                {1, 1, 0, 1, 1, 1, 0, 0, 0, 0},
-                {1, 1, 0, 0, 0, 0, 0, 1, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0, 1, 1},
-                {1, 1, 0, 0, 0, 1, 1, 0, 0, 0}
+                {0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+                {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                {1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 1, 1, 1, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 1, 1, 1, 1, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {1, 1, 0, 1, 0, 1, 1, 1, 1, 0}
         };
 
         // Display initial maze
@@ -40,17 +53,18 @@ public class MazeSolver {
     // Core recursive function that checks and updates maze indexes
     public static boolean solveMaze(int[][] maze, int row, int column) {
 
-        // Returns false if function reaches a row or column that is out of bounds of the maze dimensions
+        // (base case) Returns false if function reaches a row or column that is out of bounds of the maze dimensions
         if(row >= (maze.length) || column >= maze[0].length || row < 0 || column < 0) {
             return false;
         }
 
-        // Returns false if the index being checked is anything but a path to handle walls and checked indexes
+        // (base case) Returns false if the index being checked is anything but a path
+        // // Handles walls, previously checked indexes, and the instance where the entrance is a wall (0,0) is not 0.
         if(maze[row][column] != 0) {
             return false;
         }
 
-        // Returns true if the exit is reached marking it with 3 as part of the final path
+        // (base case) Returns true if the exit is reached marking it with 3 as part of the final path
         if(row == (maze.length - 1) && column == (maze[0].length - 1)) {
 
             maze[row][column] = 3;
@@ -61,7 +75,7 @@ public class MazeSolver {
         // This order assures that the tile would've been checked for out of bounds/walls beforehand
         maze[row][column] = 2;
 
-        // Recursively call solveMaze function to solve maze
+        // (recursive case) Recursively call solveMaze function to solve maze
         // When if statemen condition returns true, we know its index is part of the final path, so we mark it
         // Checks indexes in the order up, left, down right
         if(solveMaze(maze, row - 1, column) || solveMaze(maze, row, column -1) || solveMaze(maze, row + 1, column) ||
